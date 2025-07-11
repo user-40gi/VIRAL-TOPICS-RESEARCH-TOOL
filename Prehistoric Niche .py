6ec7ee295f53ac39f4d43778a7474fc42dcdf239
user-40gi <muhammadwaqas46730@gmail.com>
2025-07-11 13:33:51 +0500

    Update Prehistoric Niche .py

--- Prehistoric Niche .py
+++ Prehistoric Niche .py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 from datetime import datetime, timedelta
 
-API_KEY = st.secrets["API_KEY"]
+API_KEY = st.secrets["AIzaSyAIKESiph66SzOWR9LJOjkgltJJJo9MJrM"]
 
 YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 YOUTUBE_VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos"
